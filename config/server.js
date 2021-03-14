6d72f47f84118374f32861c13335fd72c0b3b714
Tatiana Velandia <tatyay@hotmail.com>
2021-03-14 15:55:19 -0500

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce373d7fc9fc294abc2b8b6540d83960'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '50e1d699-82f6-4de7-9e40-dd2096dd31e5'
+};
