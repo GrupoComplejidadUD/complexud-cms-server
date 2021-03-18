7d2a5ecac3971ab171f148ac87d398d952464e9e
Edison Peñuela <edisonpe961206@hotmail.com>
2021-03-17 22:10:41 -0500

    Use sqlite if DATABASE_URL has not been set in env

--- config/database.js
+++ config/database.js
@@ -1,26 +1,35 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  let settings = {
+    client: "sqlite",
+    filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+  };
+
+  if (env("DATABASE_URL")) {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    settings = {
+      client: "postgres",
+      host,
+      port,
+      database,
+      username: user,
+      password,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+
   return {
-    defaultConnection: 'default',
+    defaultConnection: "default",
     connections: {
       default: {
         connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
+        settings,
         options: {
           useNullAsDefault: true,
           ssl: false,
         },
       },
     },
-  }
+  };
 };
