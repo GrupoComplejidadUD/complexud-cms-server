cd15107de0398c79fc8b09b1220c7f5efc8cb8f1
Edison Peñuela <edisonpe961206@hotmail.com>
2021-08-05 23:24:52 -0500

    Change Lineas Content Type settings

--- api/lineas/controllers/lineas.js
+++ api/lineas/controllers/lineas.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.lineas.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.lineas });
+  },
+};
