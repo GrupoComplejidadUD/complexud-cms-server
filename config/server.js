6e5b06f28a85242cd3b1837fdf23fbc2af73337c
Edison Peñuela <edisonpe961206@hotmail.com>
2021-08-20 22:31:20 -0500

    Add server URL env config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("SERVER_URL", ""),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ce373d7fc9fc294abc2b8b6540d83960"),
